fb3d878f143f54588005eb42db44912cb99b58b5
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-12 16:14:30 -0500

    adding delete action for lamps, still need to finish

--- src/views/LampsShow.vue
+++ src/views/LampsShow.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="lamps-show">
+    <h1> {{message}} </h1>
     <h1>Color: {{ lamp.color }}</h1>
     <p>Size: {{ lamp.size }}</p>
     <p>Price: ${{ lamp.price }}</p>
     <router-link to="/lamps">Back to lamps</router-link>
     <div>
-    <router-link v-bind:to="`/lamps/${lamp.id}/edit`">Edit Lamp </router-link>
+    <router-link v-bind:href="`/lamps/${lamp.id}/edit`">Edit Lamp </router-link>
     </div>
+    <div>
+    <button v-on:click="destroyLamp()">Delete this bad boy</button>
+    </div>
+
 
   </div>
 </template>
@@ -20,15 +25,29 @@ import axios from "axios";
 export default {
   data: function () {
     return {
+      message: "Show stopper page!!!!!!",
       lamp: {},
     };
   },
   created: function () {
-    axios.get("/api/lamps/" + this.$route.params.id).then((response) => {
-      console.log(response.data);
-      this.lamp = response.data;
-    });
+    this.showLamp();
   },
-  methods: {},
+  methods: {
+    showLamp: function () {
+      console.log("show me the lampy");
+      console.log(this.$route);
+      axios.get("/api/lamps/" + this.$route.params.id).then((response) => {
+        console.log(response.data);
+        this.lamp = response.data;
+      });
+    },
+    destroyLamp: function () {
+      console.log("delete lamp, delete lamp!!!");
+      axios.delete(`/api/lamps/${this.$route.params.id}`).then((response) => {
+        console.log(response.data);
+        this.$router.push("/lamps");
+      });
+    },
+  },
 };
 </script>
